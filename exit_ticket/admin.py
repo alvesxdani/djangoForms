24e88128919eddd748f4d7ba32105cec4810ee0d
Dani <dani.alvesm22@gmail.com>
2021-08-18 15:36:11 -0300

    Add files from the forms project

---
+++ exit_ticket/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from exit_ticket.models import ExitTicket
+
+# Register your models here.
+
+admin.site.register(ExitTicket)
---
+++ exit_ticket/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class ExitTicket(models.Model):
+    OCUPACOES = (
+        ('CLT', 'CLT'),
+        ('PJ', 'PJ'),
+        ('EST', 'Estudante')
+    )
+
+    DUVIDAS = (
+        ('PY', 'PYTHON'),
+        ('HTML', 'HTML'),
+        ('CSS', 'CSS')
+    )
+    nome = models.CharField(max_length=200)
+    email = models.EmailField()
+    ocupacao = models.CharField(max_length=3, choices=OCUPACOES)
+    feedback = models.TextField()
+    data = models.DateField()
+    nota = models.PositiveIntegerField(default=0)
+    duvidas = models.CharField(max_length=6, choices=DUVIDAS)
+
+    data_publicacao = models.DateTimeField(auto_now_add=True)
